17324b118a4d2c1c4909eab9fd0f91d6281a81df
waste-buster <37254851+waste-buster@users.noreply.github.com>
2018-05-07 18:48:29 -0700

    Work on eval, train, and export scripts

--- research/train_rcnn.sh
+++ research/train_rcnn.sh
@@ -1,4 +1,4 @@
 python3 object_detection/train.py \
   --logtostder \
-  --pipeline_config_path=object_detection/models/cup/ssd_inception_v2_coco.config \
+  --pipeline_config_path=object_detection/waste_busters/ssd_inception_v2_cups.config \
   --train_dir=object_detection/models/cup/train/
