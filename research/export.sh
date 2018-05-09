506fe25e6fc4744e7dd42247bf69aa3afb63c61f
waste-buster <37254851+waste-buster@users.noreply.github.com>
2018-05-08 18:36:39 -0700

    Change eval script

--- research/export.sh
+++ research/export.sh
@@ -1,5 +1,10 @@
 python3 object_detection/export_inference_graph.py \
   --input_type image_tensor \
-  --pipeline_config_path=./object_detection/models/cup/faster_rcnn_resnet101_cups.config \
-  --trained_checkpoint_prefix=./object_detection/models/cup/train/model.ckpt-2683 \
-  --output_directory ./object_detection/models/cup/export/
+  --pipeline_config_path=./object_detection/waste_busters/faster_rcnn_resnet101_cups.config \
+  --trained_checkpoint_prefix=./object_detection/waste_busters/train/model.ckpt-$1 \
+  --output_directory ./object_detection/waste_busters/export/faster_rcnn_resnet101_cups_$1/
+
+tar -zcvf ./object_detection/waste_busters/export/faster_rcnn_resnet101_cups_$1.tar.gz \
+  ./object_detection/waste_busters/export/faster_rcnn_resnet101_cups_$1/*
+
+rm -r ./object_detection/waste_busters/export/faster_rcnn_resnet101_cups_$1
