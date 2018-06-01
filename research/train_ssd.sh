af484ecb545eea3bf206029dc6d5f7f29e062598
blhough <benhuff322@gmail.com>
2018-06-01 12:08:28 -0700

    Remove ssd references

--- research/train_ssd.sh
+++
@@ -1,4 +0,0 @@
-python3 object_detection/train.py \
-  --logtostder \
-  --pipeline_config_path=object_detection/waste_busters/ssd_inception_v2_cups.config \
-  --train_dir=object_detection/waste_busters/train/
